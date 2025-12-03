1ae204b8e01724dd86f232f5a9e039a4f593a61f
Nathan Longhurst <nathanlonghurst@gmail.com>
2025-12-03 11:48:22 -0700

    added kapa

--- third_model.py
+++ third_model.py
@@ -11,7 +11,7 @@ from datetime import datetime
 import calendar
 
 def solve(wind_data, max_wind_data, historic_pm10_data, surface_area_data, healthy_lake_surface_area, start_date):
-    # P'(t) = c(\alpha)S(t) - kP(t))S(t) + f(t)
+    # P'(t) = c(\alpha)S(t) - dP(t))S(t) + f(t)
     t0 = 0
     tf = min(len(wind_data), len(historic_pm10_data)) - 1
 
@@ -20,26 +20,26 @@ def solve(wind_data, max_wind_data, historic_pm10_data, surface_area_data, healt
 
     density_of_arsenic_in_lake =  5.73
     mass_of_arsenic = 74.92
-    concentration = 2/100
+    concentration = 0.18 * 1e-6
 
     def f(t):
-        max_wind_speed_t = max_wind_data[t]
-        if max_wind_speed_t >= 18:
-            exposed_surface_area = healthy_lake_surface_area - surface_area_data[t]
+        max_wind_speed_t = max_wind_data[int(round(t))]
+        if max_wind_speed_t >= 6:
+            exposed_surface_area = healthy_lake_surface_area - surface_area_data[int(round(t))]
             return (concentration*density_of_arsenic_in_lake*exposed_surface_area*max_wind_speed_t)/mass_of_arsenic
         else:
             return 0
 
 
-    def solve_pollutant_model(c, k):
-        def ode(t, p, c, k):
-            return np.array([(k*a - c*p[0])*s(t) + f(t)])
+    def solve_pollutant_model(c, d, kapa):
+        def ode(t, p, c, d, kapa):
+            return np.array([(d*a - c*p[0])*s(t) + f(t) - kapa])
 
         y0 = np.array([historic_pm10_data[0]])
 
         # solve the system (evaluate at each data point)
         n_points = tf + 1  # from 0 to tf inclusive
-        sol = solve_ivp(ode, (t0, tf), y0, t_eval=np.linspace(t0, tf, n_points), args=(c, k), method='RK45')
+        sol = solve_ivp(ode, (t0, tf), y0, t_eval=np.linspace(t0, tf, n_points), args=(c, d, kapa), method='RK45')
         if not sol.success:
             print(f"Warning: solve_ivp failed with message: {sol.message}")
         return sol
@@ -54,14 +54,16 @@ def solve(wind_data, max_wind_data, historic_pm10_data, surface_area_data, healt
         # Calculate the error
         return np.linalg.norm(diff)
 
-    result = minimize(calculate_error, [0.1, 0.1], bounds=[(0, None)])
+    result = minimize(calculate_error, [0.1, 0.1, 15], bounds=[(0, None)])
     best_c = result.x[0]
-    best_k = result.x[1]
+    best_d = result.x[1]
+    best_kapa = result.x[2]
 
     print(f"Found minimal error with c = {best_c}")
-    print(f"Found minimal error with k = {best_k}")
+    print(f"Found minimal error with d = {best_d}")
+    print(f"Found minimal error with kapa = {best_kapa}")
 
-    sol = solve_pollutant_model(best_c, best_k)
+    sol = solve_pollutant_model(best_c, best_d, best_kapa)
     plt.figure(figsize=(7, 4))
     plt.plot(sol.t, historic_pm10_data, label="Data")
     plt.plot(sol.t, sol.y[0], label="Pollutant Prediction")
@@ -69,9 +71,10 @@ def solve(wind_data, max_wind_data, historic_pm10_data, surface_area_data, healt
     plt.ylabel("ppm")
     plt.xlabel(f"Days since {start_date}")
     plt.legend()
+    plt.show()
 
-    return best_c, best_k
+    return best_c, best_d, best_kapa
 
-wind_data, pm10_recordings, surface_area_data = get_windspeed_pm10_sa("1999-01-01", "2025-12-31")
+wind_data, pm10_recordings, surface_area_data = get_windspeed_pm10_sa("2020-01-01", "2021-12-31")
 
-solve(wind_speeds)
+solve(wind_data, wind_data, pm10_recordings, surface_area_data, np.max(surface_area_data), "Jan 1. 1999")
